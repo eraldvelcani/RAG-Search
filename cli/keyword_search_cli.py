a50690d618813b930b25dfe122c595f7ee339832
eraldvelcani <eraldvelcani2003@gmail.com>
2025-10-25 14:30:47 +0200

    added basic search functionality, case sensitive for now

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-
+import json
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -17,6 +17,19 @@ def main() -> None:
             print(f'Searching for: {args.query}');
         case _:
             parser.print_help()
+    
+    with open('data/movies.json', 'r') as f:
+        movies_dict = json.load(f)
+   
+    new_dict = {}
+    
+    for movie in movies_dict['movies']:
+        if args.query in movie['title']:
+            new_dict[movie['title']] = movie
+    
+    sorted_dict = sorted(new_dict.values(), key=lambda m: m['id'])[:5]
+    for count, movie in enumerate(sorted_dict, start=1):
+        print(f"{count}. {movie['title']}")
 
 
 if __name__ == "__main__":
