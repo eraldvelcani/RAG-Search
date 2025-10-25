8273baab325b4fc6a5e6aea93f11307795de0fc7
eraldvelcani <eraldvelcani2003@gmail.com>
2025-10-25 18:34:09 +0200

    expanded on search functionality: added punct, stopwords, tokenization

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+from nltk.stem import PorterStemmer
 import argparse
 import json
+import string
 
 def main() -> None:
+    stemmer = PorterStemmer()
+
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
@@ -20,15 +24,27 @@ def main() -> None:
     
     with open('data/movies.json', 'r') as f:
         movies_dict = json.load(f)
-   
-    new_dict = {}
     
+    with open('data/stopwords.txt', 'r') as g:
+        stopwords_list = g.read().splitlines()
+
+    new_dict = {}
+    cleaned_query = (args.query).translate(str.maketrans('', '', string.punctuation)).lower()
+
     for movie in movies_dict['movies']:
-        if args.query in movie['title']:
+        cleaned_movie_title = movie['title'].translate(str.maketrans('', '', string.punctuation)).lower()
+
+        tokenized_query = list(filter(None, cleaned_query.split()))
+        tokenized_title = list(filter(None, cleaned_movie_title.split()))
+        
+        stopwordless_query = [w for w in tokenized_query if w not in stopwords_list]
+        stopwordless_title = [w for w in tokenized_title if w not in stopwords_list]
+
+        if any(q in t for q in stopwordless_query for t in stopwordless_title):
             new_dict[movie['title']] = movie
     
-    sorted_dict = sorted(new_dict.values(), key=lambda m: m['id'])[:5]
-    for count, movie in enumerate(sorted_dict, start=1):
+    sorted_dict = sorted(new_dict.values(), key=lambda m: m['id'])
+    for count, movie in enumerate(sorted_dict[:5], start=1):
         print(f"{count}. {movie['title']}")
 
 
